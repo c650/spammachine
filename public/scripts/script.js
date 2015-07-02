f2dadcc8d22ea847d9bc5504acc5a479e9e9d94b
Charles Bailey <CharlesBailey@Charless-MacBook-Pro.local>
2015-07-02 12:47:29 -0400

    added jQ stuff, still buggy

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,40 @@
 $(document).ready(function() {
-	$('#phone').click(function() {
-		$('#phone').css('height: 60px')
+	$('#phone').mouseenter(function() {
+		$('#msg').height('30px');
+		$('#msg').css('font-size', '20px');
+		$('#times').height('30px');
+		$('#times').css('font-size', '20px');
+	});
+	$('#phone').mouseleave(function() {
+		$('#msg').height('45px');
+		$('#msg').css('font-size', '35px');
+		$('#times').height('45px');
+		$('#times').css('font-size', '35px');
+	});
+
+	$('#msg').mouseenter(function() {
+		$('#phone').height('30px');
+		$('#phone').css('font-size', '20px');
+		$('#times').height('30px');
+		$('#times').css('font-size', '20px');
+	});
+	$('#msg').mouseleave(function() {
+		$('#phone').height('45px');
+		$('#phone').css('font-size', '35px');
+		$('#times').height('45px');
+		$('#times').css('font-size', '35px');
+	});
+
+	$('#times').mouseenter(function() {
+		$('#msg').height('30px');
+		$('#msg').css('font-size', '20px');
+		$('#phone').height('30px');
+		$('#phone').css('font-size', '20px');
+	});
+	$('#times').mouseleave(function() {
+		$('#msg').height('45px');
+		$('#msg').css('font-size', '35px');
+		$('#phone').height('45px');
+		$('#phone').css('font-size', '35px');
 	});
 });
