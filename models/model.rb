9b78af6e6460d04c11851b5e9ac46f11c3f50f88
gianlucazigni <gianlucazigni@gmail.com>
2015-07-02 13:11:49 -0400

    Update model.rb

--- models/model.rb
+++ models/model.rb
@@ -1,24 +1,25 @@
-require 'twilio-ruby'
-
-def send_spam(phone, msg, time)
-	time = time.to_i
-	@msg_array = Array.new
-	time.times do |x|
-		@msg_array.push(msg)
-	end
-
-	msg = @msg_array.join(' ')
-
-	account_sid = 'AC633a4f798afcb2aba4ae658ab3ddf465'
-
-	auth_token = 'dade8c6c18cad71adc0947977dd5edf7'
-
-	@client = Twilio::REST::Client.new account_sid, auth_token
-
-	 @client.account.messages.create({
-		:from => '+17862920603',  
-		:to => phone,
-		:body => msg
-	})
-
-end
+ require 'twilio-ruby'
+ 
+ def send_spam(phone, msg, time)
+ 	time = time.to_i
+ 	@msg_array = Array.new
+ 	time.times do |x|
++		break if ((msg.length)*(@msg_array.length)) > 1599
+ 		@msg_array.push(msg)
+ 	end
+ 
+ 	msg = @msg_array.join(' ')
+ 
+ 	account_sid = 'AC633a4f798afcb2aba4ae658ab3ddf465'
+ 
+ 	auth_token = 'dade8c6c18cad71adc0947977dd5edf7'
+ 
+ 	@client = Twilio::REST::Client.new account_sid, auth_token
+ 
+ 	 @client.account.messages.create({
+ 		:from => '+17862920603',  
+ 		:to => phone,
+ 		:body => msg
+ 	})
+ 
+ end
