191dad7b8cb86a08624400aac8cf817f8d07858a
Charles Bailey <CharlesBailey@Charless-MacBook-Pro.local>
2015-07-03 09:24:34 -0400

    rand array of pictures for mms

--- models/model.rb
+++ models/model.rb
@@ -1,6 +1,8 @@
 require 'twilio-ruby'
 
-def send_spam(phone, msg, time, pic_msg = 'http://41.media.tumblr.com/tumblr_lvrfnaXz651qibz0jo1_r1_500.png')
+$troll_array = ['http://41.media.tumblr.com/tumblr_lvrfnaXz651qibz0jo1_r1_500.png','http://data2.whicdn.com/images/12501601/large.jpg','http://media.tumblr.com/tumblr_m8juqvJzvz1rujvrc.jpg','http://2.bp.blogspot.com/-mqiwVnBchYI/UzchsRKNqfI/AAAAAAAADTg/BQxKnjpLgVo/s1600/Famous-characters-Troll-face-Troll-face-me-gusta-205534.png','https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT_-bWgUwY_yya6WMc3oXQYKeu2UQOJ5ptwJoePBt5CSAznNWXSDQ','https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ-aCatXehejPdb_uq4dWJMuyEQkJE-l6_9rsJjSksb4GiHfycr']
+
+def send_spam(phone, msg, time)
 	time = time.to_i
 	@msg_array = Array.new
 	time.times do |x|
@@ -20,7 +22,7 @@ def send_spam(phone, msg, time, pic_msg = 'http://41.media.tumblr.com/tumblr_lvr
 		:from => '+17862920603',  
 		:to => phone,
 		:body => msg,
-		:media_url => pic_msg
+		:media_url => $troll_array[rand($troll_array.length)]
 	})
 
 end
